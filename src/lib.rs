7ae40703d1d038abadfbe2d96c6e7b4f2def0f5c
Che <43485962+c-git@users.noreply.github.com>
2023-04-22 22:50:19 -0400

    Add docs with intentions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(test, deny(warnings))]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! # reqwest-cross
+//!
+//! The `reqwest-cross` crate provides a wrapper around [reqwest][reqwest-url] for ease of use in applications that target BOTH native and wasm.
+//! This crate is inspired by [ehttp](https://docs.rs/ehttp/0.2.0/ehttp/) but uses [reqwest][reqwest-url] instead.
+//!
+//! The currently selected approach of exposing and using [reqwest::RequestBuilder] is much more flexible and generally desirable
+//! as it allows for reuse of the same [reqwest::Client] as recommended by [reqwest][reqwest-url]. However, since it is asynchronous it requires
+//! an available runtime. For wasm the futures are handled by [wasm-bindgen-futures](https://docs.rs/wasm-bindgen-futures/latest/wasm_bindgen_futures/)
+//! but for local which runtime is specified using feature flags. If the one you want is not listed create an [issue](https://github.com/c-git/reqwest-cross/issues)
+//! and I'll attempt to add it.
+//!
+//! If you want to use the main thread for your UI and need to run [tokio](https://docs.rs/tokio/latest/tokio/) on a "secondary" thread
+//! I found this [example](https://github.com/parasyte/egui-tokio-example) helpful. I found it in this
+//! [discussion](https://github.com/emilk/egui/discussions/521), which had other suggested examples as well.
+//!
+//! [reqwest-url]: https://docs.rs/reqwest/latest/reqwest/
