92c1ac4a394325d50cad3316535fc109db26c61f
Che <43485962+c-git@users.noreply.github.com>
2023-04-23 02:14:34 -0400

    Convert the test into a doc test

--- src/native.rs
+++ src/native.rs
@@ -8,7 +8,6 @@ pub(crate) fn fetch(
 ) {
     // TODO 2: Handle case where feature is not tokio
     tokio::spawn(async move {
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await; // TODO 1: Remove sleep which test that things wait
         let result = request.send().await;
         on_done(result)
     });
--- src/wrappers.rs
+++ src/wrappers.rs
@@ -3,6 +3,35 @@
 use reqwest::{Error, RequestBuilder, Response};
 
 /// Performs a HTTP requests and calls the given callback when done.
+///
+/// # Tokio example
+/// ```rust
+///# use reqwest::{Client, Error, RequestBuilder, Response};
+///# use std::sync::mpsc::channel;
+///# use reqwest_cross::fetch;
+///
+///# #[cfg(not(target_arch = "wasm32"))] // TODO 2: Also needs to be behind the tokio feature flag
+///# #[tokio::main]
+///# async fn main(){
+///  let request = Client::new().get("http://httpbin.org/get");
+///  let (tx, rx) = channel();
+///  fetch(request, move |result: Result<Response, Error>| {
+///      tx.send(result).unwrap();
+///  });
+///  let status = loop {
+///      // Sleep too prevent locking up the thread and not awaiting
+///      tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+///      if let Ok(val) = rx.try_recv() {
+///          break val.unwrap().status();
+///      }
+///  };
+///
+///  assert_eq!(status, 200);
+///# }
+///
+///# #[cfg(target_arch = "wasm32")] // TODO 2: Also needs to be behind the tokio feature flag
+///# fn main(){}
+/// ```
 pub fn fetch(
     request: RequestBuilder,
     on_done: impl 'static + Send + FnOnce(Result<Response, Error>),
@@ -16,30 +45,7 @@ pub fn fetch(
 
 #[cfg(test)]
 mod tests {
-    use reqwest::Client;
-    use std::sync::mpsc::channel;
-
-    use super::*;
-
-    #[cfg(not(target_arch = "wasm32"))] // TODO 2: Also needs to be behind the tokio feature flag
-    #[tokio::test]
-    async fn native_get() {
-        let request = Client::new().get("http://httpbin.org/get");
-        let (tx, rx) = channel();
-        fetch(request, move |result: Result<Response, Error>| {
-            tx.send(result).unwrap();
-        });
-        let status = loop {
-            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-            if let Ok(val) = rx.try_recv() {
-                break val.unwrap().status();
-            }
-        };
-
-        assert_eq!(status, 200);
-    }
-
-    // TODO 3: Add tests for post
+    // TODO 3: Add tests for post (create as examples instead of tests)
     // TODO 3: Add tests for patch
     // TODO 3: Add tests for delete
     // TODO 3: Add tests for put
