165933904b3ef767192d66dab042abc33e92bfc6
One <43485962+c-git@users.noreply.github.com>
2024-07-16 00:49:06 -0400

    feat: relax constraints to match target
    Match constraints of the method being called.
    Was not working in WASM with constraints and they are needed.

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -1,6 +1,8 @@
 //! Stores the wrapper functions that can be called from either native or wasm
 //! code
 
+use futures::Future;
+
 /// Performs a HTTP requests and calls the given callback when done. NB: Needs
 /// to use a callback to prevent blocking on the thread that initiates the
 /// fetch. Note: Instead of calling get like in the example you can use post,
@@ -45,11 +47,7 @@ where
 
 /// Provides a cross platform compatible way to run an async function in a blocking fashion.
 /// Intended for use in callbacks as this will block but call backs are not async so we need sync code
-pub fn block_on<F>(future: F) -> F::Output
-where
-    F: std::future::Future + Send + 'static,
-    F::Output: Send + 'static,
-{
+pub fn block_on<F: Future>(future: F) -> F::Output {
     futures::executor::block_on(future)
 }
 
