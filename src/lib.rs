50cf84d4767ee343ed59da5946305715ddff0a85
One <43485962+c-git@users.noreply.github.com>
2024-12-31 21:35:25 -0500

    fix: export all of reqwest and not just client
    Needed to avoid version mismatches

--- src/lib.rs
+++ src/lib.rs
@@ -76,4 +76,4 @@ pub use wrappers::fetch;
 #[cfg(feature = "yield_now")]
 pub use yield_::yield_now;
 
-pub use reqwest::Client; // Exported to make it easier to use without a second import and maintain semver
+pub use reqwest; // Exported to make it easier to use without a second import and maintain semver
