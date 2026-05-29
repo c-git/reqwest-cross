dc7a60dce4562f97a89415d674996be5864ed0f6
One <43485962+c-git@users.noreply.github.com>
2026-05-29 15:05:30 -0400

    chore: copy lints from egui

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,1 @@
-#![deny(missing_docs)]
-#![deny(missing_debug_implementations)]
-#![warn(unused_crate_dependencies)]
-// For documenting optional features. See more at <https://c-git.github.io/rust/documentation/>
-#![cfg_attr(docsrs, feature(doc_cfg))]
-#![cfg_attr(test, deny(warnings))]
-#![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
